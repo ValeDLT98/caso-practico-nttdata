5d5acef025659a3dcf8fbb67aea40d89e6caf8a1
Valeria De la Torre <vale.delatorre.leon@gmail.com>
2024-04-28 23:46:21 -0500

    test: :green_heart: add tests

--- src/app/products/pages/add-product/add-product.component.spec.ts
+++ src/app/products/pages/add-product/add-product.component.spec.ts
@@ -2,10 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddProductComponent } from './add-product.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 describe('AddProductComponent', () => {
   let component: AddProductComponent;
   let fixture: ComponentFixture<AddProductComponent>;
+  let formBuilder: FormBuilder;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,11 +16,27 @@ describe('AddProductComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddProductComponent);
+    formBuilder = TestBed.inject(FormBuilder);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return null for undefined field', () => {
+    expect(component.getFieldError('undefinedField')).toBeNull();
+  });
+
+  it('should return error message for required field', () => {
+    const formGroup: FormGroup = formBuilder.group({
+      testField: ['', Validators.required],
+    });
+    component.myForm = formGroup;
+
+    expect(component.getFieldError('testField')).toBe(
+      'Este campo es requerido'
+    );
   });
 });
